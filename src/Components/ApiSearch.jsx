14a7c3ce6684e8cf67b023c4a33028e484d5b799
Kevin <keving3492@gmail.com>
2022-05-15 16:27:40 -0500

    switched to axios

--- src/Components/ApiSearch.jsx
+++ src/Components/ApiSearch.jsx
@@ -5,14 +5,33 @@ function ApiSearch(){
     const [ApiRes,setApiRes] = useState([])
 
     //https://api.coinlore.net/api/tickers/?start=0&limit=10
+    const axios = require('axios').default;
+
+    const getData = async () =>{
+        await axios.get('https://jsonplaceholder.typicode.com/users')
+        .then(function (response) {
+            // handle success
+            console.log(response.data)
+            setApiRes(response.data);
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+    }
     
+    /* 
+
+    This was the old method to be used using fetch()
     const getApiData = async () => {
         console.log('clicked')
          await fetch("https://jsonplaceholder.typicode.com/users")
          .then(res => res.json())
          .then(data => setApiRes(data))
          .catch(err => console.log(err))
-    }
+    } 
+    
+    */
 
    
     
@@ -23,16 +42,11 @@ function ApiSearch(){
         <div>
            <div className="search-container">
                 <input className="search" placeholder="Please enter api link"></input>
-                <div className ="button"onClick={getApiData}>Click me</div>
+                <div className ="button" onClick={getData}>Click me</div>
            </div>
            
            <div className="output-box">
-           {ApiRes.map(x => {
-               return (
-                   
-                   <div key ={x.id} className="child">{x.name}</div>
-               )
-           })}
+            
            </div>
            
            
