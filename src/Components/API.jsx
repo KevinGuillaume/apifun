5a9e9125cb686c111e558d492b569e51af24e2e1
Kevin <keving3492@gmail.com>
2022-06-15 15:13:37 -0500

    Started API section

---
+++ src/Components/API.jsx
@@ -0,0 +1,32 @@
+import "./API.css"
+
+function API(){
+    
+    
+    return (
+    <div>
+        <div className="content-container">
+            
+            <div className="content1">What is a API</div>
+            <div className="subcontent1">
+                An API (Application Programming Interface) is an interface that lets two pieces
+                of software communicate. It helps create an exchange of information based on a certain request.
+                
+            </div>
+            
+            
+            <div className="content1">Popular API Protocols</div>
+            <div className="subcontent1">
+               
+            </div>
+            <div className="subcontent1">
+              
+            </div>
+           </div>
+    </div>
+    
+    )
+        
+    
+}
+export default API
